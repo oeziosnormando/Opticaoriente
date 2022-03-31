62452e3cf4095beeb4acc496b0e80f4400f2ff06
oeziosnormando <oezios.normando@gmail.com>
2022-03-31 02:13:31 -0400

    configurando pagina do site

--- public/mail.php
+++ public/mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "esratpopy71@gmail.com";
+        $mail_to = "opticaoriente@gmail.com";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
